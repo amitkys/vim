c5badbc1863ccbe2749276a1953e6607ba2baf37
KYs <amitkys59@gmail.com>
2026-02-10 20:12:39 +0530

    removed unnessary things

--- kys.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-  int n;
-  cin >> n;
-  cout << "Hello world you have entered " << n << endl;
-  return 0;
-}
--- testing.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-  coutn << "This is testing page, handling testing"
-}
